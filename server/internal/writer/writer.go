16cd7160b747fba1b1933d74e338509976b8259b
Antonov Denis <den_ru1982@mail.ru>
2024-02-29 11:56:41 +0300

    Continue write writer

--- server/internal/writer/writer.go
+++ server/internal/writer/writer.go
@@ -1,8 +1,10 @@
 package writer
 
 import (
+	"encoding/binary"
 	"log/slog"
 	"os"
+	"sync"
 
 	"github/adminsemy/UDPSendler/server/internal/logger"
 )
@@ -12,8 +14,9 @@ type Writer struct {
 	size     int64
 	file     *os.File
 	blocks   map[uint64][]byte
-	log      *logger.Logger
-	index    uint64
+	sync.Mutex
+	log   *logger.Logger
+	index uint64
 }
 
 func New(fileName string, size int64, log *logger.Logger) (*Writer, error) {
@@ -29,10 +32,40 @@ func New(fileName string, size int64, log *logger.Logger) (*Writer, error) {
 		blocks:   make(map[uint64][]byte),
 		log:      log,
 		index:    0,
+		Mutex:    sync.Mutex{},
 	}, nil
 }
 
 func (w *Writer) Close() {
 	w.file.Close()
 	w.log.Debug("Close file:", slog.String("name", w.fileName))
+}
+
+func (w *Writer) Write(buf []byte) {
+	part := binary.LittleEndian.Uint64(buf[0:8])
+	data := buf[8:]
+	w.log.Debug("New data:", slog.Group(
+		"part",
+		slog.Uint64("part", part),
+		slog.String("data", string(data)),
+	))
+	w.Lock()
+	w.blocks[part] = data
+	w.Unlock()
+	go w.writeData()
+}
+
+func (w *Writer) writeData() {
+	w.Lock()
+	for {
+		data, ok := w.blocks[w.index]
+		if !ok {
+			break
+		}
+		w.file.Write(w.blocks[w.index])
+		w.size -= int64(len(data))
+		delete(w.blocks, w.index)
+		w.index++
+	}
+	w.Unlock()
 }
